4687fd96a4381f4323d7f2277b3fd7b965b81af1
souron-sansei <souron.sansei@gmail.com>
2017-04-13 16:31:36 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,34 +14,5 @@ ln -s -i $HOME/dotfiles-ubuntu/.config/nvim $HOME/.config/
 [ ! -d ~/.vim/bundle ] && git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 [ ! -d ~/.vim/bundle/vimproc.vim ] && git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
 
-#case "${OSTYPE}" in
-# Mac(Unix)
-#darwin*)0
-#    cd ~/.vim/bundle/vimproc.vim && make -f make_mac.mak
-#    ;;  
-# Linux
-#linux*)
-#    cd ~/.vim/bundle/vimproc.vim && make -f make_unix.mak
-#    ;;  
-#esac
-
-#vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
-
-#function askYesOrNo {
-#  MSG=$1
-#  while :
-#  do
-#    echo -n "${MSG} y/N: "
-#    read ans
-#    case $ans in
-#    [yY]) return 0 ;;
-#    [nN]) return 1 ;;
-#    esac
-#  done
-#}
-
-#askYesOrNo ">>> GUI?"
-#if [ $? -eq 0 ]; then
-#	ln -s -i $HOME/.dotfiles/.vimperatorrc $HOME/.vimperatorrc
-#	ln -s -i $HOME/.dotfiles/.vimperator $HOME/.vimperator
-#fi
+cd ~/.vim/bundle/vimproc.vim && make -f make_unix.mak
+vim -u ~/.vimrc -i NONE -c "try | NeoBundleUpdate! | finally | q! | endtry" -e -s -V1
